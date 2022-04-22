a535317cd54ed186a61a4a14e56cdf945e4ac1db
Satheesh Palanisamy <sapalan@microsoft.com>
2022-04-22 22:10:26 +0530

    Added Second Kafka Trigger and Dummy HTTP Trigger

--- spNetFunctionApp/KafkaTrigger2_Default_CGroup.cs
+++ spNetFunctionApp/KafkaTrigger2_Default_CGroup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Logging;
 
 namespace spNetFunctionApp
 {
-    public class KafkaTrigger1
+    public class KafkaTrigger2_Default_CGroup
     {
         // KafkaTrigger sample 
         // Consume the message from "topic" on the LocalBroker.
@@ -12,9 +12,9 @@ namespace spNetFunctionApp
         // For EventHubs
         // "BrokerList": "{EVENT_HUBS_NAMESPACE}.servicebus.windows.net:9093"
         // "KafkaPassword":"{EVENT_HUBS_CONNECTION_STRING}
-        [FunctionName("KafkaTrigger1_Default_CGroup")]
+        [FunctionName("KafkaTrigger2_Default_CGroup")]
         public static void Run(
-            [KafkaTrigger("pkc-7prvp.centralindia.azure.confluent.cloud:9092",
+           [KafkaTrigger("pkc-7prvp.centralindia.azure.confluent.cloud:9092",
                           "Topic1",
                           Username = "Q2L7LHNXQEYMFKRQ",
                           Password = "kYIhvBC2QOUsuCTpl02o/olvjJPI594aIgYQ9GnvybY7fp7yPYIavF6yNc8A9a9W",
@@ -24,11 +24,7 @@ namespace spNetFunctionApp
         {
             foreach (KafkaEventData<string> eventData in events)
             {
-                log.LogInformation($"C# Function 2 Kafka trigger function processed Message Start: {eventData.Value} Partition: {eventData.Partition} Offset {eventData.Offset}");
                 log.LogInformation($"C# Kafka trigger function processed a message: {eventData.Value}");
-                var vWaitingTimeInMilliSecond  =   (System.Environment.GetEnvironmentVariable("ThreadSleepTimeInMilliSeconds") == null || System.Environment.GetEnvironmentVariable("ThreadSleepTimeInMilliSeconds") =="")?3000:int.Parse(System.Environment.GetEnvironmentVariable("ThreadSleepTimeInMilliSeconds"));
-                System.Threading.Thread.Sleep(vWaitingTimeInMilliSecond);
-                log.LogInformation($"C# Function 2 Kafka trigger function processed Message End: {eventData.Value} Partition: {eventData.Partition} Offset {eventData.Offset}");
             }
         }
     }
